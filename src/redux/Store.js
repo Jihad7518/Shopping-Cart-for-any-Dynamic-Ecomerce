6c532db168dd89e48035537877f605f6feb12f44
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-31 01:28:00 +0600

    Update Store.js

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,1 +1,11 @@
 
+
+import { configureStore } from "@reduxjs/toolkit";
+import { CartSlice } from "./Slices/CartSlice";
+
+
+export const store = configureStore({
+    reducer:{
+        cart: CartSlice.reducer,
+    },
+});
