e32e2b3509cf0d99048043140b02df78e7033ee7
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-31 01:28:47 +0600

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,1 +1,16 @@
 
+import { useSelector } from "react-redux";
+import CartItem from "../components/CartItem";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+
+const Cart = () => {
+
+  const {cart} = useSelector((state) => state);
+
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect( () => {
+    setTotalAmount(cart.reduce( (acc, curr) => acc + curr.price, 0) );
+  }, [cart])
