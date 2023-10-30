17918737fc2b521f28bbe01248fedb299afd2317
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-31 01:28:59 +0600

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -14,3 +14,28 @@ const Cart = () => {
   useEffect( () => {
     setTotalAmount(cart.reduce( (acc, curr) => acc + curr.price, 0) );
   }, [cart])
+
+  return (
+      <div className="w-screen">
+
+      {
+        cart.length > 0 ? 
+        (
+          <div className="flex w-11/12 max-w-6xl p-2 mx-auto gap-7">
+            <div>
+              {
+                cart.map( (item,index) => {
+                  return <CartItem key={item.id} item={item} itemIndex={index} />
+                } )
+              }
+            </div>
+            <div className="flex flex-col w-80 h-80 justify-between mt-12 mb-6 gap-20">
+              <div className="flex flex-col gap-2">
+                <div className="mt-12">
+                <div className="text-xl font-semibold text-green-700 uppercase">Your Cart</div>
+                <div className="text-3xl font-bold text-green-700 uppercase">Summary</div>
+                </div>
+                <p>
+                  <span className="text-lg font-semibold text-green-900">Total Items: {cart.length}</span>
+                </p>
+              </div>
