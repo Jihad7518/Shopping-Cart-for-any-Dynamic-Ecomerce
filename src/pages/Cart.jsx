93e386cd9507ae417da70d791e2516b3b11df6e3
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-31 01:29:13 +0600

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -39,3 +39,30 @@ const Cart = () => {
                   <span className="text-lg font-semibold text-green-900">Total Items: {cart.length}</span>
                 </p>
               </div>
+
+              <div className="flex flex-col gap-4 mt-10 pt-10">
+                <p className="text-lg font-semibold text-green-900">Total Amount: <span className="text-lg font-bold text-green-950">${totalAmount}</span></p>
+                <button
+                className="text-white w-full rounded-md font-semibold bg-green-600
+        text-[17px] p-2 px-3 py-3 uppercase hover:bg-green-900
+        transition duration-300 ease-in"
+        >CheckOut</button>
+              </div>
+            </div>
+          </div>
+        ) :
+        (<div className="w-[100vw] h-[100vh]">
+
+         <div className="flex flex-col gap-5 text-3xl h-full justify-center items-center">
+         <div className="w-100 h-100">
+          <h1 className="text-green-700 text-lg font-bold">Your Cart is Empty</h1>
+          </div>
+
+          <div>
+          <Link to={"/"}>
+              <button className="text-white w-80 rounded-md font-semibold bg-green-600
+         p-2 px-3 py-3 uppercase hover:bg-green-900
+        transition duration-300 ease-in text-lg">
+                Shop Now
+              </button>
+          </Link>
