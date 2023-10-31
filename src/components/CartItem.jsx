42c7e5966cc644026d88cfacd933b66123443773
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-01 02:31:41 +0600

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,4 @@
 
-
 import {MdOutlineDelete} from "react-icons/md";
 import { useDispatch } from "react-redux";
 import { remove } from "../redux/Slices/CartSlice";
@@ -16,3 +15,30 @@ const CartItem = ({item, itemIndex}) => {
     toast.error("Item Removed!");
 
   }
+
+  return (
+    <div>
+
+      <div className="flex gap-4 p-4 mt-10 ml-5">
+
+        <div className="h-[200px] w-[180px]">
+          <img className="w-full h-full px-2" src={item.image}/>
+        </div>
+
+        <div className="w-80 pl-3">
+          <h1 className="text-gray-700 font-semibold text-lg
+        w-full mt-1">{item.title}</h1>
+          <h1 className="w-full text-gray-400 font-normal text-[15px]">{item.description.split(" ").slice(0, 10).join(" ") + "..."}</h1>
+          <div className="flex justify-between mt-10">
+            <p className="text-green-600 font-semibold text-lg">${item.price}</p>
+            <div onClick={removeFromCart}>
+              <MdOutlineDelete className="bg-red-300 w-9 h-9 rounded-full m-auto px-2 py-2"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
