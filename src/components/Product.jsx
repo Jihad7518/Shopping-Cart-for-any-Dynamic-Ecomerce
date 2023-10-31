2475f72c59fce864b1fb691d16e2fe83563e70b4
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-01 02:32:46 +0600

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,1 +1,20 @@
 
+import { toast } from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import {add, remove} from "../redux/Slices/CartSlice";
+
+const Product = ({post}) => {
+
+  const {cart} = useSelector( (state) => state);
+  const dispatch = useDispatch();
+
+  const addToCart = () => {
+    dispatch(add(post));
+    toast.success("Item added to Cart");
+  }
+
+  const removeFromCart = () => {
+    dispatch(remove(post.id));
+    toast.error("Item Romoved From Cart");
+  }
