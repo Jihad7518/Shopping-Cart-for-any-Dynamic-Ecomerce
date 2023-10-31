94d256e93e29e93f6f61e0fc57517c36e47b7068
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-01 02:31:16 +0600

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,1 +1,18 @@
 
+
+import {MdOutlineDelete} from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { remove } from "../redux/Slices/CartSlice";
+import { toast } from "react-hot-toast";
+
+
+
+const CartItem = ({item, itemIndex}) => {
+
+  const dispatch = useDispatch();
+
+  const removeFromCart = () => {
+    dispatch(remove(item.id));
+    toast.error("Item Removed!");
+
+  }
