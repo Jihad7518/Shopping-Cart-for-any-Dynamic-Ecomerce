e76abe6542feea22863956df1c9767688f85e193
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-01 02:30:25 +0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,3 +27,26 @@ const Home = () => {
   useEffect( () => {
     fetchProductData();
   },[])
+
+  return (
+    <div>
+      {
+        loading ? <Spinner />  :
+        posts.length > 0 ? 
+        (<div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4
+         max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
+          {
+            posts.map( (post) => (
+            <Product key = {post.id} post={post}/>
+          ) )
+          }
+        </div>) :
+        <div className="flex justify-center items-center">
+          <p>No Data Found</p>
+        </div> 
+      }
+    </div>
+  );
+};
+
+export default Home;
