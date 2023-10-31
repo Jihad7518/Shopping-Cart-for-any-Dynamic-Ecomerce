89faf032bdfa47521fdf7c4db6732d417d2a59cb
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-01 02:30:13 +0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,29 @@
 
+import { useState, useEffect } from "react";
+import Spinner from "../components/Spinner";
+import Product from "../components/Product";
+
+const Home = () => {
+  const API_URL = "https://fakestoreapi.com/products";
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  async function fetchProductData() {
+    setLoading(true);
+
+    try{
+      const res = await fetch(API_URL);
+      const data = await res.json();
+
+      setPosts(data);
+    }
+    catch(error) {
+      console.log("Error comes");
+      setPosts([]);
+    }
+    setLoading(false);
+  }
+
+  useEffect( () => {
+    fetchProductData();
+  },[])
