e0ff3709c80f65e9e0b0d279848377917556caa0
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-01 02:32:01 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,10 @@
 
+import {FaShoppingCart} from "react-icons/fa"
+import { NavLink } from "react-router-dom";
+import Cart from "../pages/Cart";
+import { useSelector } from "react-redux";
+
+const Navbar = () => {
+  const {cart} = useSelector( (state) => state);
+  return (
+    <div>
